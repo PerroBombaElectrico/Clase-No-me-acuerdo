ce7d0dd90dac3740d143a36755db896cd75f18fa
PerroBombaElectrico <pipealfredohumar@gmail.com>
2025-11-20 10:58:22 -0300

    amongus

--- Oracle_ejemplo/conexion.py
+++ Oracle_ejemplo/conexion.py
@@ -1,14 +1,17 @@
-import oracledb 
+import oracledb
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 username = os.getenv("ORACLE_USER")
 dsn = os.getenv("ORACLE_DSN")
 password = os.getenv("ORACLE_PASSWORD")
 
+
 def get_connection():
     return oracledb.connect(user=username, password=password, dsn=dsn)
+
 
 def create_schema(query):
     try:
@@ -19,39 +22,58 @@ def create_schema(query):
     except oracledb.DatabaseError as error:
         print(f"No se pudo crear la tabla: {error}")
 
-    tables = [ 
 
+tables = [
+    (
         "CREATE TABLE Dueño ("
-        "id_dueño NUMBER PRIMARY KEY,"
+        "id_dueño NUMBER PRIMARY KEY UNIQUE,"
         "nombre VARCHAR2(50),"
         "numero VARCHAR2(20),"
         "direccion VARCHAR2(100)"
-        ");"
-
+        ")"
+    )(
         "CREATE TABLE Mascota ("
         "id_mascota NUMBER PRIMARY KEY,"
         "nombre VARCHAR2(50),"
         "edad NUMBER,"
         "id_dueño NUMBER,"
         "FOREIGN KEY (id_dueño) REFERENCES Dueño(id_dueño)"
-        ");"
-
+        ")"
+    )(
         "CREATE TABLE Perro ("
         "id_mascota NUMBER PRIMARY KEY,"
         "historial_vacunas CLOB,"
         "FOREIGN KEY (id_mascota) REFERENCES Mascota(id_mascota)"
-        ");"
-
+        ")"
+    )(
         "CREATE TABLE Gato ("
-     "id_mascota NUMBER PRIMARY KEY,"
+        "id_mascota NUMBER PRIMARY KEY,"
         "registro_esterilizacion NUMBER(1),"
         "FOREIGN KEY (id_mascota) REFERENCES Mascota(id_mascota)"
-        ");"
-
+        ")"
+    )(
         "CREATE TABLE Ave ("
         "id_mascota NUMBER PRIMARY KEY,"
         "tipo_jaula VARCHAR2(30),"
         "control_vuelo VARCHAR2(30),"
         "FOREIGN KEY (id_mascota) REFERENCES Mascota(id_mascota)"
-        ");"
-    ]
+        ")"
+    )
+]
+
+
+def create_Dueño(id_dueño, nombre, numero, direccion):
+    pass
+
+
+def create_Mascota(id_mascota,id_mascotanombre,edad,id_dueño):
+    pass
+
+def create_perro(id_mascota, historial_vacunas):
+    pass
+
+def create_gato(id_mascota, registro_esterilizacion):
+    pass
+
+def create_ave(id_mascota, tipo_jaula, control_vuelo):
+    pass
