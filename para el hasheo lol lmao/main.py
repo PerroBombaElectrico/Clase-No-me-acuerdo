1ea09e79c63abe4bb3dc1415196bc7bb5db6126d
PerroBombaElectrico <pipealfredohumar@gmail.com>
2025-12-12 11:16:51 -0300

    artas cositas

--- para el hasheo lol lmao/main.py
+++ para el hasheo lol lmao/main.py
@@ -1,7 +1,10 @@
 import oracledb
-import os 
+import os
 from dotenv import load_dotenv
-import bccrypt
+import bcrypt
+from typing import Optional
+import requests
+import datetime
 
 load_dotenv()
 
@@ -9,38 +12,127 @@ username = os.getenv("ORACLE_USER")
 dsn = os.getenv("ORACLE_DSN")
 password = os.getenv("ORACLE_PASSWORD")
 
-Class Auth:
+
+class Auth:
     @staticmethod
     def register():
         pass
+
     @staticmethod
-def login():
-    pass
+    def login():
+        pass
 
 
 """
-Unidad de 
+Unidad de Fomento (UF).
+Indice de valor Promedio (IVP)
+Indice de Precio valor al consumidor (IPC)
+Unidad Tributaria mensual (UTM)
+Dólar = CLP
+
+
 """
+
 
 class Fiance:
-    @staticmethod
-    def getuf():
+    def __init__(self,base_url:str):
         pass
-    def getivp():
+    def getuf(self,fecha: str = None):
+        if not fecha:
+            year = datetime.datetime.now().year
+            month = datetime.datetime.now().month
+            day = datetime.datetime.now().day
+            fecha = f"{year}-{month}-{day}"
+            url = f"{self.base_url}/uf/{fecha}"
+            data = requests.get(url=url).json()
+            print(data['serie'][0]['valor'])
+
+
+    def getivp(self,fecha: str = None):
+        if not fecha:
+            year = datetime.datetime.now().year
+            month = datetime.datetime.now().month
+            day = datetime.datetime.now().day
+            fecha = f"{year}-{month}-{day}"
+            url = f"{self.base_url}/ivp/{fecha}"
+            data = requests.get(url=url).json():
         pass
-    def getipc():
+
+    def getipc(self,fecha: str = None):
+        if not fecha:
+            year = datetime.datetime.now().year
+            month = datetime.datetime.now().month
+            day = datetime.datetime.now().day
+            fecha = f"{year}-{month}-{day}"
+            url = f"{self.base_url}/ivp/{fecha}"
+            data = requests.get(url=url).json():
         pass
-    def getutm():
+
+    def getutm(self,fecha: str = None):
+        if not fecha:
+            year = datetime.datetime.now().year
+            month = datetime.datetime.now().month
+            day = datetime.datetime.now().day
+            fecha = f"{year}-{month}-{day}"
+            url = f"{self.base_url}/ivp/{fecha}"
+            data = requests.get(url=url).json():
+        pass(self,fecha: str = None):
+        if not fecha:
+            year = datetime.datetime.now().year
+            month = datetime.datetime.now().month
+            day = datetime.datetime.now().day
+            fecha = f"{year}-{month}-{day}"
+            url = f"{self.base_url}/ivp/{fecha}"
+            data = requests.get(url=url).json():def getusd(self,fecha: str = None):
+        if not fecha:
+            year = datetime.datetime.now().year
+            month = datetime.datetime.now().month
+            day = datetime.datetime.now().day
+            fecha = f"{year}-{month}-{day}"
+            url = f"{self.base_url}/ivp/{fecha}"
+            data = requests.get(url=url).json()::
         pass
-    def getusd():
-        pass
-    def geteur():
+
+    def geteur(self,fecha: str = None):
+        if not fecha:
+            year = datetime.datetime.now().year
+            month = datetime.datetime.now().month
+            day = datetime.datetime.now().day
+            fecha = f"{year}-{month}-{day}"
+            url = f"{self.base_url}/ivp/{fecha}"
+            data = requests.get(url=url).json()::
         pass
 
 
 class Database:
-    def__init__(self,username,password,dsn):
-    self.username = username
-    self.password = passwordself.dsn = dsn
+    def __init__(self, username, password, dsn):
+        self.username = username
+        self.password = password
+        self.passwordself.dsn = dsn
+        
+
 def get_connection(self):
     return oracledb.connect(user=self.username, password=self.password dsn=self.dsn)
+def create_all_tables(self):
+    pass
+def query(self, sentence: str, parameters: dict):
+    print(f"Ejecutando query:\n {sentence} con parametros:\n {parameters}")
+    try:
+        with self.get_connection() as connection:
+            with connection.cursor() as cursor:
+               resultado = cursor.execute(sentence, parameters)
+               for fila in resultado:
+                   print(fila)
+            connection.commit()
+    except oracledb.DatabaseError as error:
+            print(f"Hubo un error :\n {error}")
+
+
+if __name__ == "__main__":
+    db = Database(username=username, password=password, dsn=dsn)
+    db.QUERY("select sysdate from dual")
+
+    if __name__ == "__main__":
+        indicadores = Fiance()
+        indicadores.getuf()
+        indicadores.getivp()
